7c4e40834108075cd342ee0679ed29bbc4c68194
eun1310434 <eun1310434@naver.com>
2018-05-01 23:36:26 +0900

    -

--- mvvm-without-aac/src/main/java/tank/viraj/without/aac/MyApplication.kt
+++ mvvm-without-aac/src/main/java/tank/viraj/without/aac/MyApplication.kt
@@ -11,6 +11,7 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         LeakCanary.install(this)
+        //Leak Canary : akCanary 는 Open Source Java library 로 메모리 릭을 검출
         InternetUtil.init(this)
     }
 }
